6a525c258e9a7176f7bf4b8507069befd8cf6761
AraSil <122755461+CoffeeMan0923@users.noreply.github.com>
2025-01-31 12:38:02 -0300

    itemrandomspawnsystem

--- Assets/SpawnPointsManager.cs
+++ Assets/SpawnPointsManager.cs
@@ -1,41 +1,48 @@
 using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnPointsManager : MonoBehaviour
 {
     #region Editor Fields
 
-    [SerializeField] private Transform[] _spawnPoints;
-    [SerializeField] private List<GameObject> _spawnableObjects;
-    [SerializeField] Dictionary<Transform[], GameObject> _spawnableObjectsDict;
+    [SerializeField] private ItemSpawnData[] _itemAndSpawnLocacions;
+
     #endregion
 
     #region Private Fields
 
     private int _randomNumber;
+    private List<Transform> _usedSpawnLocacions;
 
     #endregion
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        foreach (Transform spawntrans in _spawnPoints)
+        _usedSpawnLocacions = new List<Transform>();
+
+        foreach (ItemSpawnData data in _itemAndSpawnLocacions)
         {
-            _randomNumber = Random.Range(0, _spawnPoints.Length);
-            Instantiate(_spawnableObjects[_randomNumber],spawntrans.position, _spawnableObjects[_randomNumber].transform.rotation);
-            spawntrans.gameObject.SetActive(false);
+            List<Transform> validSpawnPoints = data.SpawnLocacions
+                .Where(spawnPoint => spawnPoint != null && !_usedSpawnLocacions.Contains(spawnPoint))
+                .ToList();
+
+            if (validSpawnPoints.Count > 0)
+            {
+                Transform selectedSpawnPoint = validSpawnPoints[Random.Range(0, validSpawnPoints.Count)];
+                Instantiate(data.ItemPrefab, selectedSpawnPoint.position, selectedSpawnPoint.rotation);
+                _usedSpawnLocacions.Add(selectedSpawnPoint);
+            }
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    [Serializable]
+    public class ItemSpawnData
     {
-        
-    }
-    [SerializeField]
-    public class diccionary
-    {
-        public Transform _ObjSpawns;
-        public GameObject Obj;
+        public GameObject ItemPrefab;
+        public Transform[] SpawnLocacions;
     }
 }
